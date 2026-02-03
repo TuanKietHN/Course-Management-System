c3c6de6ab1ad2c59194dcedabacd9402df814404
kiett <kiettt@newwave.com.vn>
2026-02-03 14:52:59 +0700

    init

---
+++ src/main/java/vn/com/nws/cms/cms/CmsApplication.java
@@ -0,0 +1,13 @@
+package vn.com.nws.cms.cms;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CmsApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(CmsApplication.class, args);
+    }
+
+}
---
+++ src/test/java/vn/com/nws/cms/cms/CmsApplicationTests.java
@@ -0,0 +1,13 @@
+package vn.com.nws.cms.cms;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CmsApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
